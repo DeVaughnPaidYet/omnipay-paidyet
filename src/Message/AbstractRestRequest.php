da4e52256def7f6930e6a831cba578fd45a97101
san0va <sanovamedia@gmail.com>
2024-05-02 13:49:03 -0700

    remove exit

--- src/Message/AbstractRestRequest.php
+++ src/Message/AbstractRestRequest.php
@@ -189,8 +189,8 @@ abstract class AbstractRestRequest extends \Omnipay\Common\Message\AbstractReque
             // Empty response body should be parsed also as and empty array
             $body = (string) $httpResponse->getBody()->getContents();
             $jsonToArrayResponse = !empty($body) ? json_decode($body, true) : array();
-            print_r($jsonToArrayResponse);
-            exit();
+            //print_r($jsonToArrayResponse);
+            //exit();
             return $this->response = $this->createResponse($jsonToArrayResponse, $httpResponse->getStatusCode());
         } catch (\Exception $e) {
             throw new InvalidResponseException(
